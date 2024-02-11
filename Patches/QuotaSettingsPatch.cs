811b65b30ab917db87093cfb365e83ee3fbea0ea
Brandon Pugh <brandonkpugh@gmail.com>
2024-02-10 16:07:53 -0800

    Add project files.

---
+++ Patches/QuotaSettingsPatch.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using static Unity.IO.LowLevel.Unsafe.AsyncReadManagerMetrics;
+using UnityEngine;
+
+namespace CCM_DevAssistant.Patches
+{
+    [HarmonyPatch(typeof(StartMatchLever))]
+    internal class StartMatchLeverPatch
+    {
+        private static ManualLogSource mls = BepInEx.Logging.Logger.CreateLogSource(CCM_DevAssistant.DevAssistantMain.modGUID);
+
+        private static int creditsToStartWith = 10000;
+
+        [HarmonyPatch("StartGame")]
+        [HarmonyPostfix]
+        static void InfiniteStartingCredits(ref StartOfRound ___playersManager)
+        {
+            int days = ___playersManager.gameStats.daysSpent;
+
+            //mls.LogInfo((object)$"Days survived: {days}");
+            if (days > 0)
+            {
+                //mls.LogInfo((object)$"Days is greater than 0, skipping");
+                return;
+            }
+            mls.LogInfo((object)$"Setting starting credits to: {creditsToStartWith}");
+            HUDManager hudManager = (HUDManager)UnityEngine.Object.FindObjectOfType(typeof(HUDManager));
+            Terminal terminal = (Terminal)UnityEngine.Object.FindObjectOfType(typeof(Terminal));
+            terminal.SyncGroupCreditsServerRpc(creditsToStartWith, terminal.numberOfItemsInDropship);
+            mls.LogInfo((object)$"New credits value: {terminal.groupCredits}");
+            //hudManager.AddTextToChatOnServer($"Set credits: {terminal.groupCredits}", -1);
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BepInEx;
+using BepInEx.Logging;
+using CCM_DevAssistant.Patches;
+using HarmonyLib;
+
+namespace CCM_DevAssistant
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class DevAssistantMain : BaseUnityPlugin
+    {
+        public const string modGUID = "CptnsCompatibleMods.DevAssistant";
+        public const string modName = "CCM-DevAssistant";
+        public const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static DevAssistantMain Instance;
+
+        internal ManualLogSource mls;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("Loading " + modName);
+
+            harmony.PatchAll(typeof(DevAssistantMain));
+            harmony.PatchAll(typeof(StartMatchLeverPatch));
+
+            mls.LogInfo("Finished loading " + modName);
+        }
+    }
+}
